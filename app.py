12b4c1ea363e78d805c144a698fe31a09a2f2170
Paras Dhiman <55368020+dhimanparas20@users.noreply.github.com>
2023-10-16 10:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True) # Always keep debug=False for production purpose
+    app.run(host='0.0.0.0', port=5000,debug=True,threaded=True) # Always keep debug=False for production purpose
